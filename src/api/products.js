0f74e64fa4a070822ba6d72000eb310d8188f0e6
Enzo Iñigo <enzoinigo96@gmail.com>
2024-02-02 18:16:13 -0300

    agrego hover al navbar large y enlace del icono

---
+++ src/api/products.js
@@ -0,0 +1,49 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const getProductsFn = async () => {
+  const res = await fetch(`${API_URL}/products`);
+
+  if (!res.ok) {
+    throw new Error('Ocurrió un error al traer los productos!');
+  }
+
+  const data = await res.json();
+  return data;
+};
+
+export const postProductsFn = async (body) => {
+  const res = await fetch(`${API_URL}/products`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (!res.ok) {
+    throw new Error('Ocurrió un error al guardar el producto!');
+  }
+
+  const data = await res.json();
+  return data;
+};
+
+export const putProductsFn = async (data) => {
+    const res = await fetch(`${API_URL}/products/${data.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (!res.ok) {
+      throw new Error('Ocurrió un error al guardar el producto!');
+    }
+};
+
+export const deleteProductsFn = async (productId) => {
+  const res = await fetch(`${API_URL}/products/${productId}`, {
+    method: 'DELETE',
+  });
+
+  if (!res.ok) {
+    throw new Error('Ocurrió un error al eliminar el producto!');
+  }
+};
--- src/components/Common/NavbarLarge.jsx
+++ src/components/Common/NavbarLarge.jsx
@@ -6,27 +6,27 @@ const NavbarLarge = () => {
   return (
     <header className='py-3 my-3' id='large'>
     <nav className="navlarge d-flex align-items-center fixed-top">
-      <div>
+      <NavLink className={'nav-link'} to="/home">
         <img src={icono} alt="icono restaurante" width="150" height="150" />
-      </div>
-      <ul className="list-unstyled d-flex w-100 mx-1">
-      <li className="flex-fill text-center">
-          <NavLink className={`nav-link`} to="/menu">
+      </NavLink>
+      <ul className="list-unstyled d-flex w-100">
+      <li className="flex-fill text-center navCursor">
+          <NavLink className={`nav-link py-3`}  to="/menu">
             Menu
           </NavLink>
         </li>
-        <li className="flex-fill text-center">
-          <NavLink className={`nav-link`} to="/products">
+        <li className="flex-fill text-center navCursor">
+          <NavLink className={`nav-link  py-3`} to="/products">
             Products
           </NavLink>
         </li>
-        <li className="flex-fill text-center">
-          <NavLink className={'nav-link'} to="/aboutUs">
+        <li className="flex-fill text-center navCursor">
+          <NavLink className={'nav-link  py-3'} to="/aboutUs">
             About us
           </NavLink>
         </li>
-        <li className="flex-fill text-center">
-          <NavLink className={'nav-link'} to="/Login">
+        <li className="flex-fill text-center navCursor">
+          <NavLink className={'nav-link py-3'} to="/Login">
             Login
           </NavLink>
         </li>
