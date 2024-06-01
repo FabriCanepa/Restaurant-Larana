fe72aa72f0d3f4240ceb110fb9e89f8ceeb0ab5c
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2024-06-01 19:04:44 -0300

    Realizo pagina de estado de orden con opción para volver el menu

--- src/views/Status.jsx
+++ src/views/Status.jsx
@@ -1,6 +1,27 @@
+import { NavLink } from "react-router-dom";
 const Status = () => {
-  return (
-    <div>Status</div>
-  )
-}
+    return (
+        <>
+          <div className="container containerBg fw-bolder p-5 d-flex flex-column justify-content-center text-center mt-5">
+            <h3 className="m-5">Confirmed order</h3>
+            <i className="bi bi-check-circle-fill fs-5"></i>
+            <h5>Thank you for choosing us</h5>
+            <hr />
+            <div className="mt-5">
+              <h5>Your order is in the preparation process</h5>
+              <h4>Estimated time 30 mins</h4>
+            </div>
+          </div>
+          <div className="p-4 contenedor">
+            <NavLink
+              className={ (isActive) => (isActive ? "nav-link active" : "nav-link")}
+              aria-current="page"
+              to="/menu"
+            >
+              <h5 className="text-center btnStaus">New order</h5>
+            </NavLink>
+          </div>
+        </>
+      );
+    };
 export default Status
