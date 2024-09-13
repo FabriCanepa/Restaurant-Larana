6e849938bd28fa465b6a32e33491eb4f870c7a87
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2024-09-13 11:58:51 -0300

    se corrige la misma importación del archivo fotter

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -2,7 +2,7 @@
 import icono from '../../assets/Logo.png'
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import './navbarfooter.css'; 
+import "./navbarfooter.css";
 
 
 const Footer = () => {
